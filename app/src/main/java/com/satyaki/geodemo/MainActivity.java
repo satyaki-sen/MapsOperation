bd64e585fc4ae29e922484037aa1a65b26b72f7c
satyaki-sen <satyakisen1947@gmail.com>
2021-05-24 12:35:02 +0530

    Changed Camera Variable

--- app/src/main/java/com/satyaki/geodemo/MainActivity.java
+++ app/src/main/java/com/satyaki/geodemo/MainActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -25,7 +29,15 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        googleMap.addMarker(new MarkerOptions()
+
+        GoogleMap m_googleMaps=googleMap;
+
+        m_googleMaps.setBuildingsEnabled(true);
+        m_googleMaps.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(22.5726, 88.3639)));
+        m_googleMaps.setMinZoomPreference(10f);
+        m_googleMaps.setIndoorEnabled(true);
+
+        m_googleMaps.addMarker(new MarkerOptions()
                 .position(new LatLng(22.5726, 88.3639))
                 .title("Marker"));
     }
